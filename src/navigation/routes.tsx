05d771f505ba57c10b5b330af9de38490bb60c2b
FxFurEN <nikitostimoshenko@gmail.com>
2024-08-21 16:31:15 +0300

    update routes

--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -1,10 +1,7 @@
 import type { ComponentType, JSX } from 'react'
 
 import { IndexPage } from '@/pages/IndexPage/IndexPage'
-import { InitDataPage } from '@/pages/InitDataPage/InitDataPage'
 import { LaunchParamsPage } from '@/pages/LaunchParamsPage/LaunchParamsPage.tsx'
-import { ThemeParamsPage } from '@/pages/ThemeParamsPage/ThemeParamsPage.tsx'
-import { ZodiacPage } from '@/pages/ZodiacPage/page'
 
 interface Route {
 	path: string
@@ -15,9 +12,6 @@ interface Route {
 
 export const routes: Route[] = [
 	{ path: '/', Component: IndexPage },
-	{ path: '/init-data', Component: InitDataPage, title: 'Init Data' },
-	{ path: '/zodiac', Component: ZodiacPage, title: 'Zodiac page' },
-	{ path: '/theme-params', Component: ThemeParamsPage, title: 'Theme Params' },
 	{
 		path: '/launch-params',
 		Component: LaunchParamsPage,
